1f6c55bde9e86adfcf9701dd2d3691504adc4349
Mendy101 <pmmh34@gmail.com>
2025-03-18 07:42:14 +0200

    creating posts and edit post layout

--- app/src/main/java/com/example/traveltrip/MainActivity.kt
+++ app/src/main/java/com/example/traveltrip/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.profile
-        )
+        setContentView(R.layout.edit_post)
     }
 }
