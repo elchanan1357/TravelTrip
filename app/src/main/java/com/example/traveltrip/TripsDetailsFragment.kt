f284093bc1177ec7edbb3d6a82da867e2b061835
Mendy101 <pmmh34@gmail.com>
2025-04-21 16:58:18 +0300

    adding my posts layout

--- app/src/main/java/com/example/traveltrip/TripsDetailsFragment.kt
+++ app/src/main/java/com/example/traveltrip/TripsDetailsFragment.kt
@@ -5,17 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.traveltrip.databinding.MyTripDetailseBinding
+import com.example.traveltrip.databinding.MyTripDetailsBinding
 
 
 class TripsDetailsFragment : Fragment() {
-    private var binding: MyTripDetailseBinding? = null
+    private var binding: MyTripDetailsBinding?=null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = MyTripDetailseBinding.inflate(inflater, container, false)
+        binding = MyTripDetailsBinding.inflate(inflater, container, false)
 
         return binding?.root
     }
