5683c018721be0fbc16f765264071aba7e6e8815
Mendy101 <pmmh34@gmail.com>
2025-04-29 13:57:00 +0300

    add in filter

--- app/src/main/java/com/example/traveltrip/model/dao/MyTripDao.kt
+++ app/src/main/java/com/example/traveltrip/model/dao/MyTripDao.kt
@@ -5,20 +5,20 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.traveltrip.model.entity.MyTripEntity
+import com.example.traveltrip.model.entity.MyTrips
 
 
 @Dao
     interface MyTripDao {
 
         @Insert(onConflict = OnConflictStrategy.IGNORE)
-        suspend fun saveTrip(myTrip: MyTripEntity)
+        suspend fun saveTrip(myTrip:MyTrips)
 
         @Delete
-        suspend fun deleteTrip(myTrip: MyTripEntity)
+        suspend fun deleteTrip(myTrip: MyTrips)
 
         @Query("SELECT * FROM my_trips WHERE userId = :userId")
-        suspend fun getSavedTripsForUser(userId: String): List<MyTripEntity>
+        suspend fun getSavedTripsForUser(userId: String): List<MyTrips>
     }
 
 
